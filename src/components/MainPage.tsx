a1b3c95224417e6af19a066581668a9cd5100ce6
Jamie Yang <yangjm1287@gmail.com>
2024-03-10 21:28:43 -0400

    merge jack's branch and my layers components

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -4,6 +4,9 @@ import { CakeBuilder } from "./CakeBuilder";
 import { LayerAdd } from "./LayerAdd";
 import { LayerForm } from "./LayerForm";
 import { Header } from "./Header";
+import SingleCylinder from './SingleLayer';
+import SampleCake from './SampleCake';
+import AllLayers from './AllLayers';
 import '../styles/cakeBuilder.css'
 
 export function MainPage(){
@@ -15,7 +18,7 @@ export function MainPage(){
     //use state for all layers should go here
     return(
         <div className="MainPage">
-            {/* <Cake/> */}
+            <AllLayers></AllLayers>
             <div className="HeaderDiv">
                 <img className="CakeImg" src="img/cake-svgrepo-com.svg"/>
                 <Header/>
